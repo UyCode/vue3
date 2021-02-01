0924bb806d31691db5fbef9976dd552351b64918
UyCode <Hyper-Hack@outlook.com>
2021-02-01 23:58:41 +0800

    fix(change some configuration files): just for fun

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+model.export ={
+	"root": true,
+	"env": {
+		"node": true
+	},
+	"extends": [
+		"plugin:vue/vue3-essential",
+		"eslint:recommended"
+	],
+	"parserOptions": {
+		"parser": "babel-eslint"
+	},
+	"rules":{
+		"no-mixed-spaces-and-tabs": "error"
+	}
+}
