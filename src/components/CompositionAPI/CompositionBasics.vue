ebe5e47e83ac54fe273a3e8896e580285951a6dc
UyCode <Hyper-Hack@outlook.com>
2021-02-02 17:29:18 +0800

    feat(✨CompositionAPI): start with composition api 🧿

---
+++ src/components/CompositionAPI/CompositionBasics.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CompositionBasics",
+        components:{
+
+        },
+        props:{
+            user:{
+                type: String,
+                required: true
+            }
+        },
+        data(){
+            return {
+                repositories:[],
+                filters: {},
+                searchQuery: '',
+            }
+        },
+        computed:{
+            filterRepositories (){
+                // implementation
+               return  null
+            },
+            repositoriesMatchingSearchQuery(){
+                //implementation
+                return null
+            }
+        },
+        watch:{
+            user: 'getUserRepositories'
+        },
+        methods:{
+            getUserRepositories(){
+                return !!(this.repositories && this.user);
+            },
+            updateFilters(){
+                // implementation
+            }
+        },
+        mounted() {
+            this.getUserRepositories();
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
