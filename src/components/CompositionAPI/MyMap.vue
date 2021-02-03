d55eada8895f27867f93dcf5736ee166caeb23ed
UyCode <Hyper-Hack@outlook.com>
2021-02-03 12:09:15 +0800

    feat(✨Update provide and inject): 🧿

--- src/components/CompositionAPI/MyMap.vue
+++ src/components/CompositionAPI/MyMap.vue
@@ -3,14 +3,14 @@
 </template>
 
 <script>
-	import {provide} from 'vue';
+	import {provide, reactive, ref, readonly} from 'vue';
 	import MyMarker from "./MyMarker";
 	export default {
 		name: "MyMap",
 		components:{
 			MyMarker
 		},
-
+		// method 1
 		/*provide:{
 			location: 'North Pole',
 			geolocation: {
@@ -20,11 +20,41 @@
 		}*/
 
 		setup() {
-			provide('location', 'North Pole');
+			// method 2
+			/*provide('location', 'North Pole');
 			provide('geolocation', {
 				longitude: 90,
 				latitude: 135
+			});*/
+
+			//method 3, Reactivity
+			const location = ref('North Pole');
+
+			const geolocation = reactive({
+				longitude: 90,
+				latitude: 135
 			});
+
+			// this is equal to same name method in methods options
+			const updateLocation = () =>{
+				location.value = 'South Pole';
+			}
+
+			// can provide read-only, injected component not able to change the props value
+			provide('location', readonly(location));
+			provide('geolocation', readonly(geolocation));
+			provide('updateLocation', updateLocation);
+
+			return {
+				location
+			}
+		},
+
+		methods:{
+			// mutating reactivity properties
+			/*updateLocation(){
+				this.location = 'South Pole';
+			}*/
 		}
 	}
 </script>
--- src/components/CompositionAPI/MyMarker.vue
+++ src/components/CompositionAPI/MyMarker.vue
@@ -1,20 +1,43 @@
 <template>
-	<div> {{userLocation}} -> {{userGeolocation.longitude}}, {{userGeolocation.latitude}}</div>
+	<div> {{userLocation}} ->
+		{{userGeolocation.longitude}},
+		{{userGeolocation.latitude}}
+
+		<div>
+			<button @click="updateLocation"> Change Location</button>
+			<button @click="changeLocation"> Change By this Component</button>
+		</div>
+
+	</div>
+
 </template>
 
 <script>
 	import {inject} from 'vue';
 	export default {
 		name: "MyMarker",
+		//method 1
 		/*inject: ['location', 'geolocation']*/
 
 		setup(){
+			//method 2
 			const userLocation = inject('location', );
 			const userGeolocation = inject('geolocation');
+			const updateLocation = inject('updateLocation');
+
+			// try to change read-only props in there
+			// this will fail and show the reason in console
+			const changeLocation = () =>{
+				userLocation.value = 'changed by injected component';
+				console.log(userLocation.value);
+			}
+
 
 			return{
 				userLocation,
-				userGeolocation
+				userGeolocation,
+				updateLocation,
+				changeLocation
 			};
 		}
 	}
