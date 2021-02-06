bd21e3f7b57b049bcefe4e58fe1e768adaa43210
UyCode <Hyper-Hack@outlook.com>
2021-02-06 21:54:29 +0800

    feat(✨:  markdown completed): 🎡:  new feature

--- src/views/Markdown.vue
+++ src/views/Markdown.vue
@@ -1,29 +1,74 @@
 <template>
 	<section class="section-part">
-
+		<h1 style="text-align: center">Markdown App</h1>
 		<article class="left">
 			<label>
 				<textarea class="content-right"
-				          v-model="content"></textarea>
+				          :value="content"
+				          @input="update">
+
+				</textarea>
 			</label>
 		</article>
 
-		<article class="right">
-			{{content}}
+		<article class="right" v-html="markedContent">
 		</article>
+
 
 	</section>
 
 </template>
 
 <script>
+	import marked from 'marked';
+	import debounce from "../utilities/mixins/debounce";
+
 	export default {
 		name: "Markdown",
+		mixins: [debounce],
 		data(){
 			return{
-				content: ''
+				content: '',
 			}
+		},
+		computed:{
+			markedContent(){
+				return marked(this.content)
+			}
+		},
+		methods:{
+			update(e){
+				const task = () =>{this.content = e.target.value;}
+				this.debounce(task, 500);
+				},
+		},
+		beforeUnmount() {
+			alert('你确定离开此页面?');
+			/*this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+				confirmButtonText: '确定',
+				cancelButtonText: '取消',
+				type: 'warning'
+			}).then(() => {
+				this.$message({
+					type: 'success',
+					message: '删除成功!'
+				});
+			}).catch(() => {
+				this.$message({
+					type: 'info',
+					message: '已取消删除'
+				});
+			});*/
+		},
+		unmounted() {
+			setTimeout(() =>{
+				this.$message({
+					message: '你刚刚失去了一切...',
+					type: 'warning'
+				});
+			}, 1000);
 		}
+
 	}
 </script>
 
